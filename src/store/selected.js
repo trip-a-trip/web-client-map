43fb6ce25957e7b0fffa82e036bff8f5020e3754
Igor Kamyshev <igor@kamyshev.me>
2021-01-26 22:46:24 +0700

    fix: add miss first argument in selected handler

--- src/store/selected.js
+++ src/store/selected.js
@@ -4,7 +4,7 @@ const selectVenue = createEvent();
 const unselectVenue = createEvent();
 
 const $selectedVenue = createStore(null)
-  .on(selectVenue, (venue) => venue)
+  .on(selectVenue, (_, venue) => venue)
   .reset(unselectVenue);
 
 export { $selectedVenue, selectVenue, unselectVenue };
